4f2d8d0977724bc301d69a9c3574d12216afc0e9
msshmihrin <104269063+msshmihrin@users.noreply.github.com>
2022-10-12 18:12:37 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -11,6 +11,14 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     ciphertext = ""
     # PUT YOUR CODE HERE
+    keyword=(keyword*len(plaintext))[:len(plaintext)]
+    for i in range(len(plaintext)):
+        if plaintext[i] in ascii_lowercase:
+            ciphertext+=ascii_lowercase[(ascii_lowercase.find(plaintext[i]) + ascii_lowercase.find(keyword[i])) % 26]
+        elif plaintext[i] in ascii_uppercase:
+            ciphertext += ascii_uppercase[(ascii_uppercase.find(plaintext[i]) + ascii_uppercase.find(keyword[i])) % 26]
+        else:
+            ciphertext+=plaintext[i]
     return ciphertext
 
 
@@ -27,4 +35,13 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+    plaintext = ""
+    keyword = (keyword * len(ciphertext))[:len(ciphertext)]
+    for i in range(len(ciphertext)):
+        if ciphertext[i] in ascii_lowercase:
+            plaintext += ascii_lowercase[(ascii_lowercase.find(ciphertext[i]) - ascii_lowercase.find(keyword[i])+26) % 26]
+        elif ciphertext[i] in ascii_uppercase:
+            plaintext += ascii_uppercase[(ascii_uppercase.find(ciphertext[i]) - ascii_uppercase.find(keyword[i]) + 26) % 26]
+        else:
+            plaintext += ciphertext[i]
     return plaintext
