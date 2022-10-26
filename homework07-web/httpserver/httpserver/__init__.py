6a3dba07cafe78e75f1082e8847e81a069c729e6
msshmihrin <104269063+msshmihrin@users.noreply.github.com>
2022-10-26 11:56:59 +0300

    Delete homework07-web directory

--- homework07-web/httpserver/httpserver/__init__.py
+++
@@ -1,4 +0,0 @@
-from .handlers import BaseHTTPRequestHandler, BaseRequestHandler, EchoRequestHandler
-from .request import HTTPRequest
-from .response import HTTPResponse
-from .server import HTTPServer, TCPServer
